4d3f32430038b3b689abad15c8f8f7921643af4b
okesseko <59650459+okesseko@users.noreply.github.com>
2022-01-31 01:03:02 +0800

    [chore]: set color variable

--- questions/question-1.mjs
+++ questions/question-1.mjs
@@ -1,16 +1,12 @@
-import chalk from "chalk";
 import inquirer from "inquirer";
-import gradient from "gradient-string";
-import chalkAnimation from "chalk-animation";
-import figlet from "figlet";
-import { createSpinner } from "nanospinner";
 import answerHandler from "../utils/answerHandler.mjs";
+import { REACT } from "../constants/properNounColor.mjs";
 
 async function question1() {
   const answer = await inquirer.prompt({
     name: "question-1",
     type: "rawlist",
-    message: `What is ${chalk.blue("React")} ?\n`,
+    message: `What is ${REACT} ?\n`,
     choices: ["framework", "language", "library"],
   });
 
--- questions/question-2.mjs
+++ questions/question-2.mjs
@@ -1,16 +1,12 @@
-import chalk from "chalk";
 import inquirer from "inquirer";
-import gradient from "gradient-string";
-import chalkAnimation from "chalk-animation";
-import figlet from "figlet";
-import { createSpinner } from "nanospinner";
 import answerHandler from "../utils/answerHandler.mjs";
+import { REACT } from "../constants/properNounColor.mjs";
 
 async function question2() {
   const answer = await inquirer.prompt({
     name: "question-2",
     type: "rawlist",
-    message: `${chalk.blue("React")} is developed by ?\n`,
+    message: `${REACT} is developed by ?\n`,
     choices: ["Meta(Facebook)", "Google", "Individual developer"],
   });
 
