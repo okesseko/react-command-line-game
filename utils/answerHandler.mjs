e5deee225a0bfe2e313c347a8e323aae47a2765d
okesseko <59650459+okesseko@users.noreply.github.com>
2022-01-31 20:26:50 +0800

    [utils]:  add more reaction for success or fail

--- utils/answerHandler.mjs
+++ utils/answerHandler.mjs
@@ -2,17 +2,36 @@ import chalk from "chalk";
 import { createSpinner } from "nanospinner";
 import delay from "./delay.mjs";
 
-async function answerHandler(isCorrect) {
+const successMsg = [
+  "😎😎 You are right. Nice Job $user.",
+  "Well done my friend. 🤗🤗",
+  "I proud of you $user. 👏👏",
+  "Bravo! 🤩🤩",
+  "Incredible! 🙌🙌",
+];
+
+async function answerHandler(isCorrect, rightAns, userName, addToFailList) {
   const spinner = createSpinner("Checking answer...\n\n").start();
   await delay(500);
 
   if (isCorrect) {
-    spinner.success({ text: `😎😎 ${chalk.green("You are right. Nice Job")}\n` });
+    spinner.success({
+      text: `${chalk.green(
+        successMsg[Math.floor(Math.random() * 5)].replace(
+          "$user",
+          chalk.bgBlue.black(userName)
+        )
+      )}\n`,
+    });
   } else {
+    addToFailList();
     spinner.error({
-      text: `😥😥 ${chalk.redBright("Sorry, that's not the right one")}\n`,
+      text: `${chalk.redBright(
+        "😥😥 Sorry, that's not the right one."
+      )}\nThe right answer is ${chalk.bgGreen.black(rightAns)}\n`,
     });
   }
+  await delay();
 }
 
 export default answerHandler;
